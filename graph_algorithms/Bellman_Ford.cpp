a0db2de61fa07128319ed276c23319787a68920f
veenit1802 <veenitshukla20@gmail.com>
2021-09-17 15:38:56 +0530

    added graph

--- graph_algorithms/Bellman_Ford.cpp
+++ graph_algorithms/Bellman_Ford.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 
@@ -11,7 +11,7 @@ using namespace std;
     Ans     For negative cycle there no such thing as shortest path 
 
     Ques    Working of the Bellman ford algorithm
-    Ans     1) Take any anu arrangement of the graph edges.
+    Ans     1) Take any arrangement of the graph edges.
             2) Loop through the edges max(Nodes-1).
             3) If the previous node value is Infinity or INT_MAX then don't update otherwise update it.
             Note. (Works like BFS on each iteration the i distance node is reachble in minimum time from
